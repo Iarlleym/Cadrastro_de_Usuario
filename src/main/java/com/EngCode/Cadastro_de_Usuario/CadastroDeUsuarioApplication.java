99b83e44f61468351c14153b0371103f89a2349d
Iarlley <iarlleymangueira@gmail.com>
2025-10-02 14:53:07 -0300

    commit Inicial

---
+++ src/main/java/com/EngCode/Cadastro_de_Usuario/CadastroDeUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.EngCode.Cadastro_de_Usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EngCode/Cadastro_de_Usuario/CadastroDeUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EngCode.Cadastro_de_Usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
