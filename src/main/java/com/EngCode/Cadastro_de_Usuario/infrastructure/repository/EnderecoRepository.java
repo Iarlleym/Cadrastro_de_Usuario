f00a9ac897be0533ad204f6c28171f39df6a9652
Iarlley <iarlleymangueira@gmail.com>
2025-10-02 19:20:12 -0300

    Add Classes de Security

---
+++ src/main/java/com/EngCode/Cadastro_de_Usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.engcode.aprendendospring.infrastructure.repository;
+
+import com.engcode.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
